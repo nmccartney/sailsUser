5a9d4b27583b9bb01a46616acc65fde7132f5781
Nathan McCartney <nmcCartney@winsupers-MacBook-Pro.local>
2015-11-20 09:39:31 -0500

    added User CREATE action

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,5 +7,20 @@
 
 module.exports = {
 	
+	/**
+	 * CREATE action
+	 */
+	create: function (req, res, next){
+		
+		var params = req.params.all();
+		
+		User.create(params,function(err,user){
+			if(err)return next(err);
+			
+			res.status(201);
+			
+			res.json(user);
+		});
+	}
 };
 
